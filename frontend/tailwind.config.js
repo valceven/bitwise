768fb79c65fd7a73ece40ca1ebc112478e53184c
valceven <valmykelcevenbolante@gmail.com>
2025-03-27 01:53:52 +0800

    added folder structure to be discussed

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export const content = [
+    "./src/**/*.{js,jsx,ts,tsx}",
+];
+export const theme = {
+    extend: {
+        colors: {
+            accent1: '#0B6477',
+            accent2: '#0AD1C8'
+        },
+        fontFamily: {
+            //to be discussed and be chatgpted for input thanks
+        }
+    },
+};
+export const plugins = [];
