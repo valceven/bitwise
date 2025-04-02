62ba2d6d60e07bf31ceacea78fd9a58664a6b8e4
markyleangela <markyleangela@gmail.com>
2025-04-03 03:49:43 +0800

    Add SignupPage - need to fix alignments

---
+++ frontend/src/components/Stepper.jsx
@@ -0,0 +1,78 @@
+export default function Stepper({ step }) {
+    return (
+        <div className="flex items-center justify-center w-full">
+            <ol className="flex items-center justify-center w-3/4 m-10">
+                {/* Step 1 */}
+                <li
+                    className={`flex w-full items-center ${
+                        step >= 2
+                            ? "text-purple-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-purple-500 after:inline-block"
+                            : "text-gray-400 after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-300 after:inline-block"
+                    }`}
+                >
+                    <span
+                        className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${
+                            step >= 1
+                                ? "bg-purple-500 btn-shadow text-white"
+                                : "bg-gray-200 text-gray-500"
+                        }`}
+                    >
+                        1
+                    </span>
+                </li>
+
+                {/* Step 2 */}
+                <li
+                    className={`flex w-full items-center ${
+                        step >= 3
+                            ? "text-purple-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-purple-500 after:inline-block"
+                            : "text-gray-400 after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-300 after:inline-block"
+                    }`}
+                >
+                    <span
+                        className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${
+                            step >= 2
+                                ? "bg-purple-500 btn-shadow text-white"
+                                : "bg-gray-200 text-gray-500"
+                        }`}
+                    >
+                        2
+                    </span>
+                </li>
+
+                {/* Step 3 */}
+                <li
+                    className={`flex w-full items-center ${
+                        step >= 4
+                            ? "text-purple-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-purple-500 after:inline-block"
+                            : "text-gray-400 after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-300 after:inline-block"
+                    }`}
+                >
+                    <span
+                        className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${
+                            step >= 3
+                                ? "bg-purple-500 btn-shadow text-white"
+                                : "bg-gray-200 text-gray-500"
+                        }`}
+                    >
+                        3
+                    </span>
+                </li>
+
+                {/* Step 4 */}
+                <li className="flex items-center w-full">
+                    <span
+                        className={`flex items-center justify-center w-10 h-10 rounded-full lg:h-12 lg:w-12 shrink-0 ${
+                            step >= 4
+                                ? "bg-purple-500 btn-shadow text-white"
+                                : "bg-gray-200 text-gray-500"
+                        }`}
+                    >
+                        4
+                    </span>
+                </li>
+            </ol>
+        </div>
+        
+    );
+}
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,7 @@ import HomePage from "./pages/Home.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import ColorPalette from "./pages/ColorPalette.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
 
 const router = createBrowserRouter([
   {
@@ -23,7 +24,11 @@ const router = createBrowserRouter([
   {
     path: "/login",
     element: <LoginPage/>,
-  }
+  },
+  {
+    path: "/signup",
+    element: <SignupPage/>,
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
