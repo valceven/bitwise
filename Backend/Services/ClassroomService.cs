e12862fa1ad77e98b2d9fc6021b6b5f94cf81f4f
Mars0827 <marsbenitez1@gmail.com>
2025-04-10 04:58:42 +0800

    added generate classroom code

--- Backend/Services/ClassroomService.cs
+++ Backend/Services/ClassroomService.cs
@@ -15,14 +15,28 @@ namespace backend.Services
         }
 
         public async Task<Classroom> CreateClassroomAsync(CreateClassroomDTO classroomDTO)
-        {
+        {   
             var classroom = new Classroom
             {
                 ClassName = classroomDTO.ClassName,
-                TeacherId = classroomDTO.TeacherID
+                TeacherId = classroomDTO.TeacherID,
+                ClassCode = CodeGenerator.GenerateClassCode(), // Generate a random class code
             };
 
             return await _classroomRepository.CreateClassroomAsync(classroom);
+        }
+    }
+
+    // just a little helper class to generate a random class code
+    // Not included in the service interface since it's not a core functionality
+    public static class CodeGenerator
+    {
+        public static string GenerateClassCode(int length = 6)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
         }
     }
 
