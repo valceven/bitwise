77a73255a1e192534fdf31ab26883998d082f11c
valceven <valmykelcevenbolante@gmail.com>
2025-04-09 23:40:15 +0800

    fixed inconsistencies

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -13,7 +13,7 @@ function LoginPage() {
 
   if (accessToken) {
     setTimeout(() => {
-      navigate(`/profile/${user.userID}`);
+      navigate(`user/profile/${user.userID}`);
     }, 2000);
     return (
       <div className="w-full min-h-screen flex items-center justify-center">
